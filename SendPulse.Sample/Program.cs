55d80b9a8668bbde343fec446c3f95457e998661
Mark <developerslearnit@gmail.com>
2025-02-11 21:40:06 +0100

    Updated the API keys to sandbox

--- SendPulse.Sample/Program.cs
+++ SendPulse.Sample/Program.cs
@@ -6,8 +6,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddSendPulseNet(config =>
 {
     config.BaseUrl = "https://api.sendpulse.com";
-    config.ClientId = "771e5dcb9163ca4d5db3c4f971222cd3";
-    config.ClientSecret = "4a53b798c35d0eb540a7d7015f808804";
+    config.ClientId = "771e5dcb9163ca4d****3c4f971222cd3";
+    config.ClientSecret = "4a53b798c35*******a7d7015f808804";
 });
 
 
